b37711f02d1d898f0e5d2e0999d01692ade03efe
istiakahmed <istiakahmed.developer@gmail.com>
2024-11-24 00:26:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import './App.css'
+import Layout from './Layout/Layout'
+
+function App() {
+  const myRoute = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout />} >
+        <Route index element = {<Home />} />
+      </Route>
+    )
+  )
+
+  return (
+    <>
+    <RouterProvider router={myRoute} />
+      
+    </>
+  )
+}
+
+export default App
